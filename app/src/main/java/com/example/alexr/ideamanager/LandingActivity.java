a6de3fbde50857a86dbe7c9756d589b22308ad87
Vanessa Paco Ramos <vanessapr.85@gmail.com>
2019-08-13 23:47:35 -0500

    http request (#1)

--- app/src/main/java/com/example/alexr/ideamanager/LandingActivity.java
+++ app/src/main/java/com/example/alexr/ideamanager/LandingActivity.java
@@ -6,6 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.alexr.ideamanager.services.MessageService;
+import com.example.alexr.ideamanager.services.ServiceBuilder;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LandingActivity extends AppCompatActivity {
 
     @Override
@@ -13,8 +20,20 @@ public class LandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
-        TextView message = (TextView)findViewById(R.id.message);
-        message.setText("This is hardcoded, but thanks for visiting the app!  Our next hackathon is scheduled for the end of Q3.  We hope to see you there, be sure to add your ideas to the app!");
+        MessageService taskService = ServiceBuilder.buildService(MessageService.class);
+        Call<String> call = taskService.getMessages();
+
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> request, Response<String> response) {
+                ((TextView) findViewById(R.id.message)).setText(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<String> request, Throwable t) {
+                ((TextView) findViewById(R.id.message)).setText("Request Failed");
+            }
+        });
     }
 
     public void GetStarted(View view){
