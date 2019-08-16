8f50b37c6aaccea1a33442e2df966b031bf76b0e
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2019-08-16 07:25:42 -0500

    adding HTTP headers

--- app/src/main/java/com/example/alexr/ideamanager/services/ServiceBuilder.java
+++ app/src/main/java/com/example/alexr/ideamanager/services/ServiceBuilder.java
@@ -1,6 +1,14 @@
 package com.example.alexr.ideamanager.services;
 
+import android.os.Build;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -14,7 +22,22 @@ public class ServiceBuilder {
 
     // create OkHttp Client
     private static OkHttpClient.Builder okHttp =
-            new OkHttpClient.Builder().addInterceptor(logger);
+            new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request request = chain.request();
+
+                            request = request.newBuilder()
+                                    .addHeader("x-device-type", Build.DEVICE)
+                                    .addHeader("Accept-Language", Locale.getDefault().getLanguage())
+                                    .build();
+
+                            return chain.proceed(request);
+                        }
+                    })
+                    .addInterceptor(logger);
+
 
     private static Retrofit.Builder builder = new Retrofit.Builder().baseUrl(URL)
             .addConverterFactory(GsonConverterFactory.create())
